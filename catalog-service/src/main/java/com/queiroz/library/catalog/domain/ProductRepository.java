4936078629763749f874bbedaa1f0a1a3bec4bbf
Matheus <scririft@gmail.com>
2025-02-23 23:26:48 -0300

    rodando o spotless para ajeitar erros de formtação

--- catalog-service/src/main/java/com/queiroz/library/catalog/domain/ProductRepository.java
+++ catalog-service/src/main/java/com/queiroz/library/catalog/domain/ProductRepository.java
@@ -2,5 +2,4 @@ package com.queiroz.library.catalog.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {}
--- catalog-service/src/main/java/com/queiroz/library/catalog/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/queiroz/library/catalog/web/controllers/ProductController.java
@@ -1,5 +1,8 @@
 package com.queiroz.library.catalog.web.controllers;
 
+import com.queiroz.library.catalog.domain.ProductEntity;
+import com.queiroz.library.catalog.domain.ProductService;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,8 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping()
-    List<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
-        return productService.getProducts();
+    List<ProductEntity> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        // return productService.getProducts();
+        return null;
     }
 }
