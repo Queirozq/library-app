e19a59b8a226924def9e4dcce9698de038e8864b
Matheus <scririft@gmail.com>
2025-02-23 23:24:12 -0300

    arquivos de migration e arquitetura package by component

---
+++ catalog-service/src/main/java/com/queiroz/library/catalog/web/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.queiroz.library.catalog.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @GetMapping()
+    List<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        return productService.getProducts();
+    }
+}
